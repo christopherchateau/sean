7c172f3dd85e73986985ffa28b48186f4ff12a55
chris <christopherchateau@gmail.com>
2022-09-30 21:53:28 -0600

    background img onload

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,22 @@
+import { useState, useEffect } from 'react'
+import CloseUpImg from '../../assets/closeup.png';
 import './Hero.css'
 
 const Hero = () => {
+	const [imgLoaded, setImgLoaded] = useState(false)
+
+	useEffect(() => {
+		const img = new Image()
+
+		img.src = CloseUpImg
+		img.onload = () => setImgLoaded(true)
+
+	}, [CloseUpImg])
+
 	return (
-		<div className='Hero'>
-			<div className="hero-overlay" />
-			<div className="hero-text">
+		<div className='Hero' style={{ backgroundImage: `url(${CloseUpImg})` }}>
+			<div className='hero-overlay' />
+			<div className='hero-text'>
 				<h4>I hate the band Hanson</h4>
 				<h2>Sean C Thomson</h2>
 				<h3>Their hair sucks most of all it pisses me off</h3>
