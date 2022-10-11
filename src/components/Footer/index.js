a3e4450b1d1494a0f5a7134601c788f99ab66023
chris <christopherchateau@gmail.com>
2022-10-11 11:31:41 -0600

    add Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+const Footer = () => {
+	return (
+		<div className='Footer'>
+			<h4>
+				Copyright Sean C Thomson
+				<br />
+				720-329-2025
+			</h4>
+		</div>
+	)
+}
+
+export default Footer
